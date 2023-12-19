87a91ea78c2019147994ed28b6adb402f25c2fe7
M A PROGRAMMER <M A PROGRAMMER@DESKTOP-LLLBVO5>
2023-12-19 15:12:11 +0330

    Add project files.

---
+++ TeacherPayment/Program.cs
@@ -0,0 +1,16 @@
+﻿using TeacherPayment;
+
+namespace ConsoleApp1
+{
+    class test
+    {
+        public static void Main()
+        {
+            Teacher th = new Teacher("123", "Ali", "Ahmadi", 40, 40000);
+            double pay = th.Payment();
+            Console.WriteLine("Teacher payment is : {0}", pay);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ TeacherPayment/Teacher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TeacherPayment
+{
+    public class Teacher
+    {
+        private string id;
+        private string name;
+        private string lastname;
+        private int hour;
+        private int payperhour;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        public int Hour
+        {
+            get { return hour; }
+            set { hour = value; }
+        }
+
+        public int Payperhour
+        {
+            get { return payperhour; }
+            set
+            {
+                if (value > 0)
+                {
+                    payperhour = value;
+                }
+            }
+        }
+
+        public Teacher() { }
+
+        public Teacher(string Id, string Name, string Lastname, int Hour, int Payperhour)
+        {
+            id = Id;
+            name = Name;
+            lastname = Lastname;
+            hour = Hour;
+            payperhour = Payperhour;
+        }
+
+        public double Payment()
+        {
+            return hour * payperhour;
+        }
+    }
+
+}
